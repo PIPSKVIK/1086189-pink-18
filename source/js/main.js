e691fd551af97e8810b6fafa72ee8344e8f0c7bc
Alexander <52704151+PIPSKVIK@users.noreply.github.com>
2020-01-24 09:29:39 +0300

    Финальная правка с правильной работой js

--- source/js/main.js
+++ source/js/main.js
@@ -1,28 +1,9 @@
-// var open = document.querySelector(".header__burger");
-// var navigation = document.querySelector(".navigation");
+var menuBtn = document.querySelector(".navigation__toggle");
+var menu = document.querySelector(".navigation");
 
-// open.addEventListener("click", function (evt) {
-//   console.log("клик по кнопке открыть меню");
-//   navigation.classList.add("header__menu-closed");
-//   open.classList.add("header__menu-closed");
-// });
+document.documentElement.classList.add('js');
 
-var toggle = document.querySelector(".navigation__toggle");
-var menuActiv = document.querySelector(".navigation__nav-list");
-var toggleOpen = document.querySelector(".navigation__toggle-open");
-
-toggle.addEventListener("click", function (evt) {
+menuBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  console.log("сlick close");
-  toggle.classList.add("navigation__menu-activ");
-  menuActiv.classList.add("navigation__menu-activ");
-  toggleOpen.classList.add("navigation__button-activ");
-});
-
-toggleOpen.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  console.log("click open");
-  toggle.classList.remove("navigation__menu-activ");
-  menuActiv.classList.remove("navigation__menu-activ");
-  toggleOpen.classList.remove("navigation__button-activ");
+  menu.classList.toggle("navigation--active");
 });
